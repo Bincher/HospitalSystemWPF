d6878923460e4bd6b21970e03073247495e002dd
seongbin <catsb21@gmail.com>
2025-03-17 18:45:34 +0900

    초기 커밋.

---
+++ WpfApp1/Model/Doctor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WpfApp1.Models
+{
+    public class Doctor : ObservableObject
+    {
+        private int _id;
+        public int Id
+        {
+            get { return _id; }
+            set
+            {
+                SetProperty(ref _id, value);
+            }
+        }
+
+        private string? _name;
+        public string? Name
+        {
+            get { return _name; }
+            set
+            {
+                SetProperty(ref _name, value);
+            }
+        }
+
+        private string? _department;
+        public string? Department
+        {
+            get { return _department; }
+            set
+            {
+                SetProperty(ref _department, value);
+            }
+        }
+
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                SetProperty(ref _age, value);
+            }
+        }
+    }
+}
---
+++ WpfApp1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.Models;
+using WpfApp1.ViewModel.Bases;
+
+namespace WpfApp1.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private IList<Doctor>? _doctor;
+        public IList<Doctor>? Doctor
+        {
+            get => _doctor;
+            set => SetProperty(ref _doctor, value);
+        }
+
+        private Doctor? _selectedPerson;
+        public Doctor? SelectedPerson
+        {
+            get => _selectedPerson;
+            set => SetProperty(ref _selectedPerson, value);
+        }
+
+        public MainWindowViewModel()
+        {
+            Doctor = new List<Doctor>
+            {
+                new Doctor{Id = 111111, Name = "김지찬", Department = "정형외과", Age = 31},
+                new Doctor{Id = 111112, Name = "이재현", Department = "정형외과", Age = 30},
+                new Doctor{Id = 111113, Name = "김영웅", Department = "성형외과", Age = 35},
+                new Doctor{Id = 112111, Name = "박병호", Department = "성형외과", Age = 44},
+                new Doctor{Id = 112112, Name = "류지혁", Department = "피부과", Age = 52},
+            };
+        }
+
+    }
+}
